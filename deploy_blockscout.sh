e20498610fbc404183ad85717efb7f319eee09a0
Evgeny Koshelev <eko2000@gmail.com>
2024-08-02 22:42:39 +0200

    Add blockscout deployment stuff

--- deploy_blockscout.sh
+++ deploy_blockscout.sh
@@ -27,14 +27,24 @@ pushd blockscout_clone
 git clone https://github.com/blockscout/blockscout
 popd
 
+# Copy docker data to a separate directory
+echo_stage "Copy docker data to a separate directory"
+test -d ../lestnet_data || mkdir -p ../lestnet_data
+test -d ../lestnet_data/docker || cp -r blockscout_clone/blockscout/docker ../lestnet_data
+test -d ../lestnet_data/docker-compose || cp -r blockscout_clone/blockscout/docker-compose ../lestnet_data
+
 # Apply config patches
 echo_stage "Apply Config Patches"
-sudo cp blockscout-patch/common-blockscout.env.patch blockscout_clone/blockscout/docker-compose/envs/common-blockscout.env
-sudo cp blockscout-patch/docker-compose.yml.patch blockscout_clone/blockscout/docker-compose/docker-compose.yml
-sudo cp blockscout-patch/user-ops-indexer.yml.patch blockscout_clone/blockscout/docker-compose/services/user-ops-indexer.yml
+sudo cp blockscout-patch/common-blockscout.env.patch ../lestnet_data/docker-compose/envs/common-blockscout.env
+sudo cp blockscout-patch/docker-compose.yml.patch ../lestnet_data/docker-compose/docker-compose.yml
+sudo cp blockscout-patch/user-ops-indexer.yml.patch ../lestnet_data/docker-compose/services/user-ops-indexer.yml
 
 # Deploy docker images
 echo_stage "Deploy Blockscout Docker Images"
-pushd blockscout_clone/blockscout/docker-compose
+pushd ../lestnet_data/docker-compose
 sudo docker-compose up -d
 popd
+
+# Remove cloned blockscout repo
+echo_stage "Remove cloned blockscout repo"
+sudo rm -rf blockscout_clone
