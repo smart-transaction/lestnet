82ed714f46da113178c363bda52507f8365cc1ba
Evgeny Koshelev <eko2000@gmail.com>
2024-08-03 13:29:18 +0200

    Add config patch

--- getting-started-patch/getting-started-config.sh
+++ getting-started-patch/getting-started-config.sh
@@ -96,8 +96,7 @@ config=$(cat << EOL
 
   "faultGameAbsolutePrestate": "0x03c7ae758795765c6664a5d39bf63841c71ff191e9189522bad8ebff5d4eca98",
   "faultGameMaxDepth": 44,
-  "faultGameClockExtension": 0,
-  "faultGameMaxClockDuration": 600,
+  "faultGameMaxDuration": 1200,
   "faultGameGenesisBlock": 0,
   "faultGameGenesisOutputRoot": "0x0000000000000000000000000000000000000000000000000000000000000000",
   "faultGameSplitDepth": 14
