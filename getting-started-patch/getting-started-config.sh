59523a49852f34f1a55bde5a8380de4fa4d98969
Evgeny Koshelev <eko2000@gmail.com>
2024-08-03 13:04:56 +0200

    Add config patch

--- getting-started-patch/getting-started-config.sh
+++ getting-started-patch/getting-started-config.sh
@@ -43,7 +43,7 @@ config=$(cat << EOL
   "channelTimeout": 300,
 
   "p2pSequencerAddress": "$GS_SEQUENCER_ADDRESS",
-  "batchInboxAddress": "0xff000000000000000000000000000000000${L2_CHAIN_ID}",
+  "batchInboxAddress": "0xff00000000000000000000000000000000021363",
   "batchSenderAddress": "$GS_BATCHER_ADDRESS",
 
   "l2OutputOracleSubmissionInterval": 120,
